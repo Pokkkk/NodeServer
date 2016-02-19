c8a54e9d9d9161905d0163d5d7a029bfda68c729
Pokkkk <c6369@naver.com>
2016-02-19 12:54:46 +0900

    안쓰는 메소드 주석처리

--- index.js
+++ index.js
@@ -51,47 +51,47 @@ io.sockets.on('connection', function (socket) {
     });
 
     // when the client emits 'add user', this listens and executes
-    socket.on('add user', function (username) {
-        if (addedUser) return;
-
-        // we store the username in the socket session for this client
-        socket.username = username;
-        ++numUsers;
-        addedUser = true;
-        socket.emit('login', {
-            numUsers: numUsers,
-        });
-        // echo globally (all clients) that a person has connected
-        socket.to(roomId).emit('user joined', {
-            username: socket.username,
-            numUsers: numUsers
-        });
-    });
-
-    // when the client emits 'typing', we broadcast it to others
-    socket.on('typing', function () {
-        socket.to(roomId).emit('typing', {
-            username: socket.username
-        });
-    });
-
-    // when the client emits 'stop typing', we broadcast it to others
-    socket.on('stop typing', function () {
-        socket.to(roomId).emit('stop typing', {
-            username: socket.username
-        });
-    });
-
-    // when the user disconnects.. perform this
-    socket.on('disconnect', function () {
-        if (addedUser) {
-            --numUsers;
-
-            // echo globally that this client has left
-            socket.to(roomId).emit('user left', {
-                username: socket.username,
-                numUsers: numUsers
-            });
-        }
-    });
+    //socket.on('add user', function (username) {
+    //    if (addedUser) return;
+    //
+    //    // we store the username in the socket session for this client
+    //    socket.username = username;
+    //    ++numUsers;
+    //    addedUser = true;
+    //    socket.emit('login', {
+    //        numUsers: numUsers,
+    //    });
+    //    // echo globally (all clients) that a person has connected
+    //    socket.to(roomId).emit('user joined', {
+    //        username: socket.username,
+    //        numUsers: numUsers
+    //    });
+    //});
+    //
+    //// when the client emits 'typing', we broadcast it to others
+    //socket.on('typing', function () {
+    //    socket.to(roomId).emit('typing', {
+    //        username: socket.username
+    //    });
+    //});
+    //
+    //// when the client emits 'stop typing', we broadcast it to others
+    //socket.on('stop typing', function () {
+    //    socket.to(roomId).emit('stop typing', {
+    //        username: socket.username
+    //    });
+    //});
+    //
+    //// when the user disconnects.. perform this
+    //socket.on('disconnect', function () {
+    //    if (addedUser) {
+    //        --numUsers;
+    //
+    //        // echo globally that this client has left
+    //        socket.to(roomId).emit('user left', {
+    //            username: socket.username,
+    //            numUsers: numUsers
+    //        });
+    //    }
+    //});
 });
